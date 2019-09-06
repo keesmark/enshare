06f0d4f253311dba90a010009703c763077db73c
katsu <keesmark@gmail.com>
2019-09-06 15:29:50 -0600

    axios post

--- src/store/actions/gearActions.js
+++ src/store/actions/gearActions.js
@@ -1,5 +1,17 @@
+import axios from "axios";
+
+const apiUrl = "http://localhost:3000";
+
 export const createGear = gear => {
   return (dispatch, getState) => {
-    dispatch({ type: "CREATE_GEAR", gear });
+    axios
+      .post(`${apiUrl}/gears`, { gear })
+      .then(response => {
+        dispatch({ type: "CREATE_GEAR", gear });
+        console.log(response);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 };
